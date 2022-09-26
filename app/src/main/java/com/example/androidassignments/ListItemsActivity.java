daf38f502580a9fad856d4774ca63ced70418d39
Shailendra Singh <computerface56@hotmail.com>
2022-09-25 20:28:36 -0400

    Added 3 activities for part 2

---
+++ app/src/main/java/com/example/androidassignments/ListItemsActivity.java
@@ -0,0 +1,14 @@
+package com.example.androidassignments;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ListItemsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_items);
+    }
+}
