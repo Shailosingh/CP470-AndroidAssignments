9965e6c46edffb7b4581580b372e5766f73d759f
Shailendra Singh <computerface56@hotmail.com>
2022-10-23 23:57:28 -0400

    Not even close to finished Part 4 but, must submit

---
+++ app/src/main/java/com/example/androidassignments/TestToolbar.java
@@ -0,0 +1,82 @@
+package com.example.androidassignments;
+
+import android.os.Bundle;
+
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.example.androidassignments.databinding.ActivityTestToolbarBinding;
+
+public class TestToolbar extends AppCompatActivity
+{
+
+    private AppBarConfiguration appBarConfiguration;
+    private ActivityTestToolbarBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityTestToolbarBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        setSupportActionBar(binding.toolbar);
+
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_test_toolbar);
+        appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+
+        binding.fab.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    @Override
+    public boolean onSupportNavigateUp()
+    {
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_test_toolbar);
+        return NavigationUI.navigateUp(navController, appBarConfiguration)
+                || super.onSupportNavigateUp();
+    }
+
+    public boolean onCreateOptionsMenu (Menu m)
+    {
+        getMenuInflater().inflate(R.menu.toolbar_menu, m );
+        return true;
+    }
+
+    public boolean onOptionsItemSelected(MenuItem mi)
+    {
+        switch(mi.getItemId())
+        {
+            case R.id.action_one:
+                Log.d("Toolbar", "Option 1 selected");
+                break;
+
+            case R.id.action_two:
+                //Start an activity...
+                break;
+
+            case R.id.action_three:
+                break;
+        }
+    }
+}
